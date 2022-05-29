4b69cd1adfdba82902138aad418a9f77a29668b1
kingnaldoo <reinaldosn2016@gmail.com>
2022-05-29 02:44:02 -0300

    inicial structure

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
+
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/icons/favicon.png" />
+
+    <?php wp_head(); ?>
+</head>
+<body>
+    <header>
+
+    </header>
