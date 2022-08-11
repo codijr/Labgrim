ecb10a406814a186388f5cca61ad952210babb58
felipemartins8058 <felipecosmemartins10@gmail.com>
2022-08-11 20:54:57 -0300

    Adding dynamic content to Front page

--- front-page.php
+++ front-page.php
@@ -2,26 +2,37 @@
 
 <main>
     <section class="position-relative w-100" id="banner">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/test-banner.png" />
-        <div class="filter"></div>
+        <?php 
+            $args = array(
+                'post_type' => 'noticia',
+                'posts_per_page' => 1
+            );
+            $query = new WP_Query( $args );
+            while ( $query -> have_posts()) : $query-> the_post();
+        ?>
+            <img src="<?php echo get_the_post_thumbnail_url() ?>" />
+            <div class="filter"></div>
 
-        <div class="position-absolute d-flex" id="pattern-group">
-            <img class="pattern" id="x-1" src="<?php echo get_template_directory_uri(); ?>/assets/icons/x-pattern.svg" />
-            <img class="pattern" id="triangle-1" src="<?php echo get_template_directory_uri(); ?>/assets/icons/triangle-pattern.svg" />
-            <img class="pattern" id="x-2" src="<?php echo get_template_directory_uri(); ?>/assets/icons/x-pattern.svg" />
-            <img class="pattern" id="circle-1" src="<?php echo get_template_directory_uri(); ?>/assets/icons/circle-pattern.svg" />
-            <img class="pattern" id="x-3" src="<?php echo get_template_directory_uri(); ?>/assets/icons/x-pattern.svg" />
-        </div>
-
-        <div class="position-absolute w-100 d-flex flex-column align-items-center" id="info">
-            <div class="container w-100 d-flex flex-column align-items-center">
-                <button class="bg-news p-0">
-                    <p class="tag px-3 py-1 text-white">Notícia</p>
-                </button>
-                
-                <h1 class="text-center text-white">A adoção responsável pode fazer uma enorme diferença na vida de crianças e adolescentes</h1>
+            <div class="position-absolute d-flex" id="pattern-group">
+                <img class="pattern" id="x-1" src="<?php echo get_template_directory_uri(); ?>/assets/icons/x-pattern.svg" />
+                <img class="pattern" id="triangle-1" src="<?php echo get_template_directory_uri(); ?>/assets/icons/triangle-pattern.svg" />
+                <img class="pattern" id="x-2" src="<?php echo get_template_directory_uri(); ?>/assets/icons/x-pattern.svg" />
+                <img class="pattern" id="circle-1" src="<?php echo get_template_directory_uri(); ?>/assets/icons/circle-pattern.svg" />
+                <img class="pattern" id="x-3" src="<?php echo get_template_directory_uri(); ?>/assets/icons/x-pattern.svg" />
             </div>
-        </div>
+
+            <div class="position-absolute w-100 d-flex flex-column align-items-center" id="info">
+                <div class="container w-100 d-flex flex-column align-items-center">
+                    <button class="bg-news p-0">
+                        <p class="tag px-3 py-1 text-white">Notícia</p>
+                    </button>
+                    
+                    <a href="<?php echo get_permalink(); ?>">
+                        <h1 class="text-center text-white"><?php echo get_the_title() ?></h1>
+                    </a>
+                </div>
+            </div>
+        <?php endwhile; ?>
     </section>
 
     <section class="container w-100" id="other-news">
@@ -29,29 +40,29 @@
             <div class="container pt-5 pb-3">
                 <div class="swiper-news mb-3">
                     <div class="swiper-wrapper">
-                        <div class="swiper-slide text-center px-4">
-                            <p class="tag mb-2 text-news">Notícia</p>
-                            <a href="#"><h5>A adoção responsável pode fazer uma enorme diferença na vida de crianças e adolescentes</h5></a>
-                        </div>
-
-                        <div class="swiper-slide text-center px-4">
-                            <p class="tag mb-2 text-news">Notícia</p>
-                            <a href="#"><h5>A arerentes</h5></a>
-                        </div>
-
-                        <div class="swiper-slide text-center px-4">
-                            <p class="tag mb-2 text-news">Notícia</p>
-                            <a href="#"><h5>A adoção responsáqwerweescentes</h5></a>
-                        </div>
+                        <?php 
+                            $args = array(
+                                'post_type' => 'noticia',
+                                'posts_per_page' => 3,
+                                'offset' => 1
+                            );
+                            $query = new WP_Query( $args );
+                            while ( $query -> have_posts()) : $query-> the_post();
+                        ?>
+                            <div class="swiper-slide text-center px-4">
+                                <p class="tag mb-2 text-news">Notícia</p>
+                                <a href="<?php echo get_permalink(); ?>"><h5><?php echo get_the_title() ?></h5></a>
+                            </div>
+                        <?php endwhile; ?>
                     </div>
                     <div class="swiper-pagination mb-5"></div>
                 </div>
 
                 <div class="w-100 d-flex justify-content-center">
-                    <button class="bg-news px-4 py-2">
+                    <a class="bg-news px-4 py-2" href="<?php echo get_home_url(); ?>/noticias">
                         <p class="tag text-white">VER MAIS</p>
-                    </button>
-                </div>
+                    </a>
+                </div>  
             </div>
         </div>
     </section>
@@ -62,19 +73,20 @@
                 <h2 class="text-production mb-5">Produções</h2>
 
                 <div class="mb-5" id="list-production">
-                    <?php includeFile('components/card-production.php', array(
-                        'image' => get_template_directory_uri() . '/assets/img/test-production.png',
-                        'category' => 'AUDIOVISUAL',
-                        'title' => 'Publicidade Infantil',
-                        'subtitle' => 'Filme realizado pelo LabGRIM, alunos da disciplinda de Produção Audiovisual para Crianças e Adolescentes e Fundação Casa Grande.'
-                    )) ?>
-
-                    <?php includeFile('components/card-production.php', array(
-                        'image' => get_template_directory_uri() . '/assets/img/test-production.png',
-                        'category' => 'AUDIOVISUAL',
-                        'title' => 'Publicidade Infantil',
-                        'subtitle' => 'Filme realizado pelo LabGRIM, alunos da disciplinda de Produção Audiovisual para Crianças e Adolescentes e Fundação Casa Grande.'
-                    )) ?>
+                    <?php 
+                        $args = array(
+                            'post_type' => 'producao'
+                        );
+                        $query = new WP_Query( $args );
+                        while ( $query -> have_posts()) : $query-> the_post();
+                    ?>
+                        <?php includeFile('components/card-production.php', array(
+                            'image' =>  get_the_post_thumbnail_url(),
+                            'category' => get_the_category( $id )[0]->name ,
+                            'title' => get_the_title(),
+                            'subtitle' => get_the_content()
+                        )) ?>
+                    <?php endwhile; ?>
                 </div>
             </section>
 
@@ -82,19 +94,21 @@
                 <h2 class="text-schedule mb-1">Agenda de atividades</h2>
 
                 <div id="list-schedule">
-                    <?php includeFile('components/card-schedule-home.php', array(
-                        'image' => get_template_directory_uri() . '/assets/img/test-schedule.png',
-                        'title' => 'Entrevista com Edgar Maçal',
-                        'date' => '25/10/2021',
-                        'time' => '13:30'
-                    )) ?>
-
-                    <?php includeFile('components/card-schedule-home.php', array(
-                        'image' => get_template_directory_uri() . '/assets/img/test-schedule.png',
-                        'title' => 'Entrevista com Edgar Maçal',
-                        'date' => '25/10/2021',
-                        'time' => '13:30'
-                    )) ?>
+                    <?php 
+                        $args = array(
+                            'post_type' => 'agendadeatividades'
+                        );
+                        $query = new WP_Query( $args );
+                        while ( $query -> have_posts()) : $query-> the_post();
+                    ?>
+                        <?php includeFile('components/card-schedule-home.php', array(
+                            'image' => get_the_post_thumbnail_url(),
+                            'title' => get_the_title(),
+                            'date' => get_field('data_da_atividade'),
+                            'time' => get_field('hora_da_atividade'),
+                            'subtitle' => get_the_content()
+                        )) ?>
+                    <?php endwhile; ?>
                 </div>
             </aside>
         </div>
