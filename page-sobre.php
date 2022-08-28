a7de3368d9918a36c0ed882257ea00621f03121a
kingnaldoo <reinaldosn2016@gmail.com>
2022-08-28 12:21:49 -0300

    Add page sobre and styles

---
+++ page-sobre.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<main>
+    <section class="container bg-about py-5" id="title">
+        <h1 class="text-white">Integrantes</h1>
+    </section>
+</main>
+
+<?php get_footer(); ?>
